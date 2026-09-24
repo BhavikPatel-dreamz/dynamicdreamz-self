8d6b57f29e39f31494613c6b364bff5f5744a67b
Vatsal <vatsal.rangoonwala926@gmail.com>
2026-09-24 18:18:47 +0530

    removed br

--- src/content/fabric-theme-customization.ts
+++ src/content/fabric-theme-customization.ts
@@ -19,7 +19,7 @@ export const fabricThemeCustomizationContent = {
     },
   },
   brands: {
-    title: "Trusted by <br>Leading Brands",
+    title: "Trusted by Leading Brands",
     items: industryBrandLogos,
   },
   features: {
